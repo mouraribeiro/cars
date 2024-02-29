149db3ccf2aea99b4f34b67e44ece15d4f44e840
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2024-02-29 16:47:36 -0300

    criação dos models e config de media root

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Car,Brand
 
 # Register your models here.
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    
+    
+    
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ['model','brand']
+
+
+
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)
