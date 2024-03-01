65fe6aa966bf2007ed8c8cc831bbf9dcb886db00
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2024-03-01 17:31:14 -0300

    ajustando fluxo de templates e views

--- cars/models.py
+++ cars/models.py
@@ -17,7 +17,7 @@ class Car(models.Model):
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
-    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True )
     
     def __str__(self):
         return self.model
