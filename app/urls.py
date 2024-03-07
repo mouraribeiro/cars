814813e75e795cbbb5fc39e74b8e8103e4d230d0
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2024-03-07 17:30:06 -0300

    criando o forms de new car

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import cars
+from cars.views import cars, new_car_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/',cars, name='cars_list')   
+    path('cars/',cars, name='cars_list'),
+    path('newcar/',new_car_view, name='new_cars')   
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import CarForm
 
 
 # Create your views here.
@@ -8,3 +9,9 @@ from cars.models import Car
 def cars(request):
     cars = Car.objects.all()
     return render(request, 'cars.html', {'cars': cars})
+
+
+
+def new_car_view(request):
+    new_car_form = CarForm()
+    return render(request, 'new_car.html', {'new_car_form':new_car_form})
