5d0d194ada453c342ba87d5ee06bf52b888952ab
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2024-03-08 17:23:08 -0300

    inicniando o cadastro de usuários

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars, new_car_view
+from accounts.views import register_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_view, name='register'),
     path('',cars, name='cars_list'),
     path('newcar/',new_car_view, name='new_cars')   
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
